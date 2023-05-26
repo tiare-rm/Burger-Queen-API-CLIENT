9608145382023cc8460eae7d4354681d0195cf49
Tiare Rojas Madariaga <tiarerojasmadariaga_@hotmail.com>
2023-05-26 10:47:24 -0400

    vite funcionando, imagenes, componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import routes from routes.jsx
+import 'index.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// componente inicial que mantiene el root de mi proyecto 
+ReactDOM.createRoot(document.getElementById("root")).render(<routes />);
